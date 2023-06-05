88f5c47fa9b30e4e69af411778117a1b19ffddab
yurioneix <yurioneix18@gmail.com>
2023-06-05 18:03:54 -0300

    Requisito 4: cria classe abstrata Archetype e seus atributos

---
+++ src/Archetype.ts
@@ -0,0 +1,13 @@
+abstract class Archetype {
+  private _name: string;
+  private _special: number;
+  private _cost: number;
+
+  constructor(name: string) {
+    this._name = name;
+    this._cost = 0;
+    this._special = 0;
+  }
+}
+
+export default Archetype;
