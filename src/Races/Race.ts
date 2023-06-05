afb950b7889749d705c9fd26902ebba53467ccd0
yurioneix <yurioneix18@gmail.com>
2023-06-05 14:51:22 -0300

    Requisito 1: criando classe abstrata Race, com os atributos name e dexterity e iniciando no construtor

---
+++ src/Races/Race.ts
@@ -0,0 +1,11 @@
+abstract class Race {
+  private _name: string;
+  private _dexterity: number;
+
+  constructor(name: string, dexterity: number) {
+    this._name = name;
+    this._dexterity = dexterity;
+  }
+}
+
+export default Race;
