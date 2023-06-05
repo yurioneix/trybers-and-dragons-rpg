2186d51e3524694e3f5d0eab5a101477cf49be67
yurioneix <yurioneix18@gmail.com>
2023-06-05 18:08:08 -0300

    Requisito 4: cria getters e o método abstrato createdArchetypeInstances

--- src/Archetype.ts
+++ src/Archetype.ts
@@ -1,12 +1,28 @@
 abstract class Archetype {
-  private _name: string;
-  private _special: number;
-  private _cost: number;
+  private readonly _name: string;
+  private readonly _special: number;
+  private readonly _cost: number;
 
   constructor(name: string) {
     this._name = name;
     this._cost = 0;
     this._special = 0;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get special() {
+    return this._special;
+  }
+
+  get cost() {
+    return this._cost;
+  }
+
+  static createdArchetypeInstances(): number {
+    throw new Error('Not implemented');
   }
 }
 
