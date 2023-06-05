144d00176e954ce3ce1c4908877a8ca196411aa8
yurioneix <yurioneix18@gmail.com>
2023-06-05 17:05:01 -0300

    Requisito 3: cria inteface Energy e o tipo EnergyType

---
+++ src/Energy.ts
@@ -0,0 +1,10 @@
+// import { EnergyType } from './types/EnergyType';
+
+export type EnergyType = 'mana' | 'stamina';
+
+interface Energy {
+  type_: EnergyType;
+  amount: number; 
+}
+
+export default Energy;
