36e54a2eda129d4699a1084e96255b19621e4541
yurioneix <yurioneix18@gmail.com>
2023-06-05 14:58:06 -0300

    Requisito 1: cria getters para os atributos da classe Race e seu arquivo index

--- src/Races/Race.ts
+++ src/Races/Race.ts
@@ -1,10 +1,18 @@
 abstract class Race {
-  private _name: string;
-  private _dexterity: number;
+  private readonly _name: string;
+  private readonly _dexterity: number;
 
   constructor(name: string, dexterity: number) {
     this._name = name;
     this._dexterity = dexterity;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get dexterity() {
+    return this._dexterity;
   }
 }
 
