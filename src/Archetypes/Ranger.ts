a005bd9e5fa5425fc40f9949483ae5cea0ae0384
yurioneix <yurioneix18@gmail.com>
2023-06-05 18:42:53 -0300

    Requisito 5: cria classe Warrior

---
+++ src/Archetypes/Ranger.ts
@@ -0,0 +1,24 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+class Ranger extends Archetype {
+  private _energyType: EnergyType;
+  private static _instancesCount = 0;
+
+  constructor(name: string) {
+    super(name);
+    this._energyType = 'stamina';
+
+    Ranger._instancesCount += 1;
+  }
+
+  static createdArchetypeInstances(): number {
+    return this._instancesCount;
+  }
+
+  get energyType(): EnergyType {
+    return this._energyType;
+  }
+}
+
+export default Ranger;
