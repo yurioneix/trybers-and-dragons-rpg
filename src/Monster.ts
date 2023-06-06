1b4d00df96ccaaffba5a59cfb50ccb51be2fa44d
yurioneix <yurioneix18@gmail.com>
2023-06-06 15:43:58 -0300

    Requisito 9: cria getters para os atributos de Monster e implementa o método receiveDamage

--- src/Monster.ts
+++ src/Monster.ts
@@ -1,7 +1,36 @@
 import { SimpleFighter } from './Fighter';
 
 class Monster implements SimpleFighter {
+  private _lifePoints: number;
+  private _strength: number;
 
+  constructor() {
+    this._lifePoints = 85;
+    this._strength = 63;
+  }
+
+  get lifePoints(): number {
+    return this._lifePoints;
+  }
+
+  get strength(): number {
+    return this._strength;
+  }
+
+  attack(enemy: SimpleFighter): void {
+    throw new Error('Method not implemented.');
+  }
+
+  receiveDamage(attackPoints: number): number {
+    const damage = attackPoints - this._lifePoints;
+    this._lifePoints -= damage;
+
+    if (this._lifePoints === 0) {
+      this._lifePoints = -1;
+    }
+    
+    return this._lifePoints;
+  }
 }
 
 export default Monster;
